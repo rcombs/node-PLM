61f50d805d0926d24167d070d56e72a18b02bd24
Rodger Combs <rodger.combs@gmail.com>
2013-04-05 00:04:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+OK, I swear I'll actually document this. It's complicated, and definitely needs it, but testing it takes priority at the moment.
+
+VERY EXPERIMENTAL.
+AS OF v0.0.0, ABSOLUTELY NOTHING IS TESTED
---
+++ test.js
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+var PLM = require("./"),
+	readline = require("readline");
+
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	completer: completer,
+	terminal: true
+});
+
+var modem = new PLM(process.argv[2]);
+
+function completer(line){
+	return [[], ""];
+}
+
+var commands = {
+	sendCommand: function(hex){
+		modem.sendCommand(new Buffer(hex, "hex"), function(ACK, cmd, data){
+			rl.write("ACK:" + ACK + "; CMD:" + cmd + "; DATA: " + data);
+			rl.prompt();
+		});
+	},
+	sendDirect: function(to, cmd, hops){
+		modem.sendINSTEON({
+			to: new Buffer(to, "hex"),
+			cmd: new Buffer(cmd, "hex"),
+			hops: parseInt(hops, 10)
+		}, function(response){
+			rl.write(JSON.stringify(response));
+			rl.prompt();
+		});
+	}
+};
+
+rl.on("line", function(line){
+	var args = line.split(" ");
+	if(args[0] in commands){
+		commands[args[0]].apply(this, args.slice(1));
+	}else{
+		rl.write("Command not found: " + args[0] + ". Type `help` for a list.");
+		rl.prompt();
+	}
+});
